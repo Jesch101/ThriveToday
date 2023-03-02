1d546ee0b02f028dea4ed41d42ab0ba4b2d74cbb
tmtrinidad <0trinidad6@gmail.com>
2023-03-02 14:12:41 -0800

    delete get

--- dev/backend/src/routes/signup-routes.js
+++ dev/backend/src/routes/signup-routes.js
@@ -3,10 +3,6 @@ const controller = require("./controller");
 
 const router = Router(); 
 
-router.get("/", (req,res) => {
-    res.send("Sign up :3");
-});
-
 router.post("/", controller.addUser);
 
 module.exports = router; // Export router and import to server.js
